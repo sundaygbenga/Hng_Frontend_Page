f932b746213421364cf60921ec776bb90e58b68e
Sunday Gbenga <sunflizasher@gmail.com>
2023-09-08 20:42:21 +0100

    dynamic UTC update

--- index.js
+++ index.js
@@ -9,32 +9,37 @@ slack.innerHTML = slackName;
 track.innerHTML = stack;
 
 // Date Api
-const now = new Date();
 
-const options = {
-	weekday: 'long',
+const updateTime = () => {
+	const now = new Date();
+
+	const options = {
+		weekday: 'long',
+	};
+
+	// const option = {
+	// 	hour: 'numeric',
+	// 	minute: 'numeric',
+	// 	second: 'numeric',
+	// 	// day: 'numeric',
+	// 	// month: 'long', // long, 2-digit,short
+	// 	// year: 'numeric',
+	// 	// weekday: 'long',
+	// };
+
+	// getting the locale from user browser
+	const locale = navigator.language;
+	// console.log(locale);
+
+	const labelDay = new Intl.DateTimeFormat(locale, options).format(now);
+	day.innerHTML = `${labelDay} -`;
+
+	// const labelTime = new Intl.DateTimeFormat(locale, option).format(now);
+	// time.innerHTML = labelTime;
+
+	// const utcTime = now.getTime();
+	const utcTime = Date.now();
+	time.innerHTML = utcTime;
 };
 
-// const option = {
-// 	hour: 'numeric',
-// 	minute: 'numeric',
-// 	second: 'numeric',
-// 	// day: 'numeric',
-// 	// month: 'long', // long, 2-digit,short
-// 	// year: 'numeric',
-// 	// weekday: 'long',
-// };
-
-// getting the locale from user browser
-const locale = navigator.language;
-// console.log(locale);
-
-const labelDay = new Intl.DateTimeFormat(locale, options).format(now);
-day.innerHTML = `${labelDay} -`;
-
-// const labelTime = new Intl.DateTimeFormat(locale, option).format(now);
-// time.innerHTML = labelTime;
-
-// const utcTime = now.getTime();
-const utcTime = Date.now();
-time.innerHTML = utcTime;
+setInterval(updateTime, 1000);
